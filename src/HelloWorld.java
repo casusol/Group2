342ce7c0814acedabec5de60502e8cfbf237c8ba
Casuso Carrion <luchocasuso@gmail.com>
2019-04-10 16:25:25 -0700

    change name

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Create a Hello World Application in Java and push it
 		System.out.println("Hello World Application in Java!");
-		System.out.println("Add my name, Lu!");
+		System.out.println("Add my name, Lu Casuso!");
 	}
 
 }
